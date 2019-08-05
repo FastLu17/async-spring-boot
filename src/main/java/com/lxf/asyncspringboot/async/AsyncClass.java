8b9e4bb7f49d21f075d05bb713107a0d180226db
xiao66 <948632050@qq.com>
2019-08-05 14:50:35 +0800

    使用Executor线程池和@Async分别实现异步操作

---
+++ src/main/java/com/lxf/asyncspringboot/async/AsyncClass.java
@@ -0,0 +1,69 @@
+package com.lxf.asyncspringboot.async;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+
+/**
+ * @author 小66
+ * @create 2019-08-05 13:41
+ **/
+
+@Component
+public class AsyncClass {
+
+    @Async
+    public void asyncMethod_1() throws InterruptedException {
+        long timeMillis = System.currentTimeMillis();
+        Thread.sleep(1000);
+        System.out.println("asyncMethod_1 totalMillis = " + (System.currentTimeMillis() - timeMillis));
+    }
+
+    @Async
+    public void asyncMethod_2() throws InterruptedException {
+        long timeMillis = System.currentTimeMillis();
+        Thread.sleep(2000);
+        System.out.println("asyncMethod_2 totalMillis = " + (System.currentTimeMillis() - timeMillis));
+    }
+
+    @Async
+    public void asyncMethod_3() throws InterruptedException {
+        long timeMillis = System.currentTimeMillis();
+        Thread.sleep(3000);
+        System.out.println("asyncMethod_3 totalMillis = " + (System.currentTimeMillis() - timeMillis));
+    }
+
+    /**
+     *  利用spring的异步操作,必须使用@Async和@EnableAsync注解
+     * @param sleep
+     */
+    @Async
+    public void asyncMethod_4(Long sleep) {
+        long timeMillis = System.currentTimeMillis();
+        try {
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("asyncMethod_" + sleep + " totalMillis = " + (System.currentTimeMillis() - timeMillis));
+    }
+
+    /**
+     *  利用线程池进行异步操作时,不需要@Async和@EnableAsync注解
+     * @param sleep
+     */
+    public void threadAsyncMethod(Long sleep) {
+        Executors.newCachedThreadPool().execute(() -> {//没有线程池的时候,即属于Sync的操作、
+            long timeMillis = System.currentTimeMillis();
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("threadAsyncMethod_" + sleep + " totalMillis = " + (System.currentTimeMillis() - timeMillis));
+        });
+    }
+
+
+}
---
+++ src/main/java/com/lxf/asyncspringboot/controller/AsyncController.java
@@ -0,0 +1,79 @@
+package com.lxf.asyncspringboot.controller;
+
+import com.lxf.asyncspringboot.async.AsyncClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 小66
+ * @create 2019-08-05 13:44
+ **/
+@RestController
+public class AsyncController {
+
+    private List<Long> longList;
+
+    @Autowired
+    private AsyncClass asyncClass;
+
+    @GetMapping("/annotation")
+    public String startAnnotationAsync() throws InterruptedException {
+        System.out.println("longList = " + longList.size());
+        long timeMillis = System.currentTimeMillis();
+        asyncClass.asyncMethod_3();
+        asyncClass.asyncMethod_2();
+        asyncClass.asyncMethod_1();
+        System.out.println("EndAsync totalMillis = " + (System.currentTimeMillis() - timeMillis));
+        return "AnnotationAsync 耗时 :" + (System.currentTimeMillis() - timeMillis);
+    }
+
+    @GetMapping("/thread")
+    public String startThreadAsync() {
+        long startMillis = System.currentTimeMillis();
+        List<Long> longList = getListSync();
+        System.out.println("同步获取List的时间 = " + (System.currentTimeMillis()-startMillis));
+        long timeMillis = System.currentTimeMillis();
+        for (Long aLong : longList) {
+            System.out.println("aLong = " + aLong);
+            asyncClass.threadAsyncMethod(aLong);
+        }
+
+        System.out.println("EndAsync totalMillis = " + (System.currentTimeMillis() - timeMillis));
+
+        return "ThreadAsync 耗时 :" + (System.currentTimeMillis() - timeMillis);
+    }
+
+    @GetMapping
+    public String index() {
+        long startMillis = System.currentTimeMillis();
+        this.longList = getListAsync();
+        System.out.println("异步获取List的时间 = " + (System.currentTimeMillis()-startMillis));
+        //此时longList.size()=0;其他方法再获取此longList对象时,异步操作已经生成10万数据.
+        System.out.println("longList = " + longList.size());
+        return "Async 耗时 :" + (System.currentTimeMillis() - startMillis);
+    }
+
+    private static List<Long> getListSync() {
+        List<Long> longList = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            longList.add(Math.abs(new Random().nextInt(1000)) + 2000L);
+        }
+        return longList;
+    }
+
+    private static List<Long> getListAsync() {
+        List<Long> longList = new ArrayList<>();
+        Executors.newCachedThreadPool().execute(()->{
+            for (int i = 0; i < 100000; i++) {
+                longList.add(Math.abs(new Random().nextInt(1000)) + 2000L);
+            }
+        });
+        return longList;
+    }
+}
